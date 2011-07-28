bf8417a3cdfea62311271e725fb46f7673998cf2
Alex Garcia <hello@agevio.com>
2011-07-28 13:09:09 -0500

    fixed base url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://rowndly.local';
+$config['base_url']	= 'http://rowndly.com';
 
 /*
 |--------------------------------------------------------------------------
