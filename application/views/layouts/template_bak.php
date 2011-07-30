2f0057229948716f97410150f17b41ab7c9af5cb
Alex Garcia <hello@agevio.com>
2011-07-29 20:18:11 -0500

    Work on front end [d:production]

--- application/views/layouts/template_bak.php
+++ application/views/layouts/template_bak.php
@@ -8,10 +8,9 @@
 </head>
 <body>
 	<div id="message"></div>
-	
 	<div id="header">
 		<div id="logo-wrapper">
-			<a href="/">rowndly</a>
+			<a href="/"><img src="/assets/images/rowndly-logo-small.jpg" alt="rowndly" id="logo"/></a>
 		</div>
 		<div id="account-wrapper">
 			<?php if ($this->auth->is_logged_in()):?>
@@ -25,11 +24,11 @@
 		</div>
 	<div class="clearfix"></div>
 	</div>
-	
 	<div id="wrapper">
-		<?php echo $content?>
+		<div id="content-left">
+			<?php echo $content?>
+		</div>
 	</div>
-	
 	<div id="footer">rowndly - &copy; 2011 - agevio</div>
 <script src="/assets/js/jquery-1.5.1.min.js"></script>
 <script src="/assets/js/jquery-ui-1.8.14.custom.min.js"></script>
--- application/views/rownds/index.php
+++ application/views/rownds/index.php
@@ -1,59 +1,66 @@
 <?php
+echo app::div_open(array('id'=>'add-rownd-wrapper'));
+	echo form_open('rownds/create', array('id'=>'form'));
+	echo form_input(array('name'=>'url','id'=>'url', 'value'=>'enter a url'));
+	echo form_submit('submit','Add Rownd', 'id="add-rownd"');
+	echo app::div('<img src="/assets/images/loader.gif" alt="loader"/>', array('class'=>'loader'));
+	echo form_close();
+echo app::div_close();
 
-if (! $allow_public)
-{
-	echo '<ul id="syndication-options">';
-		echo '<li>'. anchor('/api/json/'. $user_id .'/'. $private_key, 'json', array('target'=>'_blank')) .'</li>';
-		echo '<li>'. anchor('/api/xml/'. $user_id .'/'. $private_key, 'xml', array('target'=>'_blank')) .'</li>';
-		echo '<li>'. anchor('/api/rss/'. $user_id.'/'. $private_key, 'rss', array('target'=>'_blank')) .'</li>';
-	echo '</ul>';
-}
-else
-{
-	echo '<ul id="syndication-options">';
-		echo '<li>'. anchor('/api/json/'. $user_id , 'json', array('target'=>'_blank')) .'</li>';
-		echo '<li>'. anchor('/api/xml/'. $user_id, 'xml', array('target'=>'_blank')) .'</li>';
-		echo '<li>'. anchor('/api/rss/'. $user_id, 'rss', array('target'=>'_blank')) .'</li>';
-	echo '</ul>';	
-}
+echo app::div_open(array('id'=>'content-left'));
+	if (! $allow_public)
+	{
+		echo '<ul id="syndication-options">';
+			echo '<li>'. anchor('/api/json/'. $user_id .'/'. $private_key, 'json', array('target'=>'_blank')) .'</li>';
+			echo '<li>'. anchor('/api/xml/'. $user_id .'/'. $private_key, 'xml', array('target'=>'_blank')) .'</li>';
+			echo '<li>'. anchor('/api/rss/'. $user_id.'/'. $private_key, 'rss', array('target'=>'_blank')) .'</li>';
+		echo '</ul>';
+	}
+	else
+	{
+		echo '<ul id="syndication-options">';
+			echo '<li>'. anchor('/api/json/'. $user_id , 'json', array('target'=>'_blank')) .'</li>';
+			echo '<li>'. anchor('/api/xml/'. $user_id, 'xml', array('target'=>'_blank')) .'</li>';
+			echo '<li>'. anchor('/api/rss/'. $user_id, 'rss', array('target'=>'_blank')) .'</li>';
+		echo '</ul>';	
+	}
+echo app::div_close();
 
-echo form_open('rownds/create', array('id'=>'form'));
-echo form_input(array('name'=>'url','id'=>'url', 'value'=>'enter a url'));
-echo form_submit('submit','Add Rownd', 'id="add-rownd"');
-echo app::div('<img src="/assets/images/loader.gif" alt="loader"/>', array('class'=>'loader'));
-echo form_close();
-echo anchor('#','Edit', array('class'=>'edit-mode like-button'));
-
+echo app::div_open(array('id'=>'content-right'));
 if (isset($rownds) and is_array($rownds))
 {
-	echo '<ul id="sortable">';
-	echo '<li class="empty"></li>';
-	foreach($rownds as $rownd)
-	{
-		$edit = anchor('/rownds/edit/'. $rownd->id,
-			'<img src="/assets/images/pencil.png" alt="Edit Rownd"/>', 
-			array('class'=>'edit-link','rel'=>$rownd->id));
+		echo anchor('#','Edit', array('class'=>'edit-mode like-button'));
+		echo '<ul id="sortable">';
+		echo '<li class="empty"></li>';
+		foreach($rownds as $rownd)
+		{
+			$edit = anchor('/rownds/edit/'. $rownd->id,
+				'<img src="/assets/images/pencil.png" alt="Edit Rownd"/>', 
+				array('class'=>'edit-link','rel'=>$rownd->id));
 		
-		$delete = anchor('/rownds/destroy/'. $rownd->id, 
-			'<img src="/assets/images/minus.png" alt="Delete Rownd"/>', 
-			array('class'=>'delete-link','rel'=>$rownd->id));
+			$delete = anchor('/rownds/destroy/'. $rownd->id, 
+				'<img src="/assets/images/minus.png" alt="Delete Rownd"/>', 
+				array('class'=>'delete-link','rel'=>$rownd->id));
 			
-		echo '<li class="ui-state-default" id="rownd_'. $rownd->id .'">';
-		echo '<div id="title_url_for_'. $rownd->id .'"/>';
-		echo anchor($rownd->url, character_limiter($rownd->title, 70), 
-					array('id'=>'anchor_title_for_'.$rownd->id,'target'=>'_blank','class'=>'rownd-link','rel'=>$rownd->id));
-		echo app::div($rownd->url, array('class'=>'rownd-url', 'id'=>'anchor_url_for_'.$rownd->id));
-		echo app::div('Last Rownd: '. date("m/d/Y @ h:i a", strtotime($rownd->last_visited)), array('class'=>'last-visited','id'=>'last_visited_for_'.$rownd->id));
-		echo '</div>';
+			echo '<li class="ui-state-default" id="rownd_'. $rownd->id .'">';
+			echo '<div id="title_url_for_'. $rownd->id .'"/>';
+			echo anchor($rownd->url, character_limiter($rownd->title, 70), 
+						array('id'=>'anchor_title_for_'.$rownd->id,'target'=>'_blank','class'=>'rownd-link','rel'=>$rownd->id));
+			echo app::div($rownd->url, array('class'=>'rownd-url', 'id'=>'anchor_url_for_'.$rownd->id));
+			echo app::div('Last Rownd: '. date("m/d/Y @ h:i a", strtotime($rownd->last_visited)), array('class'=>'last-visited','id'=>'last_visited_for_'.$rownd->id));
+			echo '</div>';
 		
-		echo '<form method="post" action="/rownds/update" class="inline-form" id="form_for_'. $rownd->id .'">
-		<input type="input" value="'. $rownd->title .'" name="inline-title" class="inline-title"/>
-		<input type="input" value="'. $rownd->url .'" name="inline-url" class="inline-url"/>
-		<input type="hidden" value="'.$rownd->id.'" name="inline-id" id="id_for_'.$rownd->id.'"/>
-		<input type="submit" value="Save" class="inline-submit"/>
-		</form>';
-		echo $edit; 
-		echo $delete .'</li>';
-	}
-	echo '</ul>';
+			echo '<form method="post" action="/rownds/update" class="inline-form" id="form_for_'. $rownd->id .'">
+			<input type="input" value="'. $rownd->title .'" name="inline-title" class="inline-title"/>
+			<input type="input" value="'. $rownd->url .'" name="inline-url" class="inline-url"/>
+			<input type="hidden" value="'.$rownd->id.'" name="inline-id" id="id_for_'.$rownd->id.'"/>
+			<input type="submit" value="Save" class="inline-submit"/>
+			</form>';
+			echo $edit; 
+			echo $delete .'</li>';
+		}
+		echo '</ul>';
 }
+echo app::div_close();
+
+echo app::div('', array('class'=>'clearfix'));
