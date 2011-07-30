14a85d5ea8ad473567806d5184a834c48859d4f8
Alex Garcia <hello@agevio.com>
2011-07-30 13:34:09 -0500

    trying to get springloops to deploy my code to production

--- application/views/layouts/login.php
+++ application/views/layouts/login.php
@@ -13,7 +13,6 @@
 		<div id="logo"><img src="/assets/images/logo-small.gif" alt="rowndly"></div>
 		<?php echo $content?>
 	</div>
-	
 	<div id="footer">rowndly - &copy; 2011 - agevio</div>
 <script src="/assets/js/jquery-1.5.1.min.js"></script>
 <script src="/assets/js/jquery-ui-1.8.14.custom.min.js"></script>
