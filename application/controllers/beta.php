eb67ac505faad0ea41ff78e26e4e02d29cb99d66
agevio <hello@agevio.com>
2011-08-05 14:11:27 -0500

    work on invite email

--- application/controllers/beta.php
+++ application/controllers/beta.php
@@ -22,4 +22,17 @@ class Beta extends Public_Controller{
 		$this->db->insert('beta', $user);
 		redirect('/beta/index/1');
 	}
+	
+	public function send_invite($email = 'codersatx@gmail.com')
+	{
+		$this->load->helper('send_email');
+		$message = '<p>Hi. Thank you for your interest in our private beta.</p>';
+		$message .= '<p>Below you will find a link to register. Feel free to register';
+		$message .= 'and play with the service. If you find any bugs please submit them to:</p>';
+		$message .= '<p>ticket+rowndly.80138-6engsjz6@lighthouseapp.com</p>';
+		$message .= '<p>Register for Rowndly:</p>';
+		$message .= '<p>http://rowndly.com/register</p>';
+		$message .= '<p>Alex Garcia</p>';
+		send_email('Alex Garcia', 'codersatx@gmail.com', $email, 'Rowndly Invite', $message);
+	}
 }
---
+++ application/helpers/send_email_helper.php
@@ -0,0 +1,36 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Preps an email message and sends it.
+*
+* @param string $from_name :the name of the person the email is coming from 
+* @param string $from_email :the email address of the person sending the email
+* @param string $to_email :the email address of the recipient
+* @param string $subject :the subject of the email message
+* @param string $message :the body of the message being sent
+* @return boolean TRUE if sent FALSE if not
+* @uses send_email('Some Name','fromemail@isp.com','toemail@isp.com','Some Message Subject','The Message Body')
+*/
+function send_email($from_name,$from_email,$to_email,$subject,$message)
+{
+    $ci = get_instance();
+    $ci->load->library('email');
+
+    $config['protocol'] = 'mail';
+    $config['smtp_host'] = 'mail.alexgarciafineart.com';
+    $config['smtp_user'] = 'alex@alexgarciafineart.com';
+    $config['smtp_pass'] = 'corner927';
+    $config['charset'] = 'iso-8859-1';
+    $config['wordwrap'] = TRUE;
+    $config['mailtype'] = 'html';
+
+    $ci->email->initialize($config);
+    $ci->email->from($from_email, $from_name);
+    $ci->email->to($to_email);
+    $ci->email->subject($subject);
+    $ci->email->message($message);
+
+    if($ci->email->send())
+    {
+        return TRUE;
+    }
+}
