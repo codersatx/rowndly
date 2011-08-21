999c264e9ceeb2fed81f71d152c71b645f6073a5
Alex Garcia <hello@agevio.com>
2011-08-20 23:06:51 -0500

    added support for posting from ipad app:

--- post.php
+++ post.php
@@ -4,7 +4,7 @@ $url = $_GET['url'];
 $url = parse_url($url);
 $host = $url['host'];
 $path = str_replace('/', '--', $url['path']);
+$ipad = isset($_GET['ipad']) ? $_GET['ipad'] : '';
 
-
-header('Location: /rownds/post/'.$host.'/'.$path);
+header('Location: /rownds/post/'.$host.'/'.$path.'/'. $ipad);
 
