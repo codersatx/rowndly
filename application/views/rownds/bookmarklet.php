94638bd2aefed5cd88d04d4c88ffdd0179905f48
Alex Garcia <hello@agevio.com>
2011-09-04 18:38:27 -0500

    More front end fixes

--- application/views/rownds/bookmarklet.php
+++ application/views/rownds/bookmarklet.php
@@ -4,14 +4,14 @@
 javascript:javascript:(function(){var%20url%20=%20location.href;window.open('http://rowndly.com/post.php?url='%20+%20encodeURIComponent(url)%20+%20'&','_blank','location=0,menubar=no,height=130,width=650,toolbar=no,scrollbars=no,status=no');})();
 </textarea>
 
-<h3>To add this bookmarklet to your iPad bookmars bar follow these steps:</h3>
+<h3>To add this bookmarklet to your iPad bookmarks bar follow these steps:</h3>
 <ol>
 	<li>Bookmark this page.</li>
-	<li>Copy and Paste the code in the textbox above.</li>
+	<li>Copy and paste the code in the textbox above.</li>
 	<li>Click the bookmark icon in the browser bar above.</li>
-	<li>Navigation to bookmarks and then the bookmarks bar list</li>
-	<li>Click edit and select the Add to Rownds bookmark.</li>
-	<li>Click in the url box and delete the current bookmark.</li>
+	<li>Navigate to the Bookmarks and then the Bookmarks Bar section.</li>
+	<li>Click Edit and select the Add to Rownds bookmark.</li>
+	<li>Click inside the url input box and delete the current bookmark.</li>
 	<li>Paste the code you copied from step 2 into that same box.</li>
 	<li>Enjoy.</li>
 </ol>
