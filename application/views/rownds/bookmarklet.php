b5e6978170861dc24f09030f99ea529721fb8718
Alex Garcia <hello@agevio.com>
2011-09-04 18:17:29 -0500

    Work on bookmarklet code for ipad

--- application/views/rownds/bookmarklet.php
+++ application/views/rownds/bookmarklet.php
@@ -2,6 +2,6 @@
 <div class="single-column">
 <h2>Bookmarklet Code for iPad</h2>
 <textarea style="width:99%;height:170px;font-size:18px">
-<a href="javascript:javascript:(function(){var%20url%20=%20location.href;window.open('http://rowndly.com/post.php?url='%20+%20encodeURIComponent(url)%20+%20'&','_blank','location=0,menubar=no,height=130,width=650,toolbar=no,scrollbars=no,status=no');})();" title="Drag this to your bookmark bar.">Add To Rowndly</a>
+javascript:javascript:(function(){var%20url%20=%20location.href;window.open('http://rowndly.com/post.php?url='%20+%20encodeURIComponent(url)%20+%20'&','_blank','location=0,menubar=no,height=130,width=650,toolbar=no,scrollbars=no,status=no');})();" 
 </textarea>
 </div>
